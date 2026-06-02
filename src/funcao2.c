3d235763ef9f6752ac4b7872a4647f7f67c7a61e
Pedrinho <dueldnf@gmail.com>
2026-06-02 09:39:18 -0300

    add: trabalhando com ponteiros

--- src/funcao2.c
+++ src/funcao2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "funclayout.h"
+#include "../lib/funclayout.h"
+#include "../lib/funcsoma.h"
 
 int main (){
     int x , y , z;
@@ -8,5 +9,8 @@ int main (){
     scanf("%d",&x);
     printf("digite outro numero inteiro e tecle enter\n");
     scanf("%d",&y);
-
+    z = soma(x,y);
+    printf("o resultado da soma e %d\n", z);
+    rodape();
+    return 0;
 }
